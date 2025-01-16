20b8abe9bbf1b1306ce13fc485f575aabc7d4ae2
Tassnim <s90301@bht-berlin.de>
2025-01-16 13:09:32 +0100

    fertig

--- src/Pages/LandingPage.tsx
+++ src/Pages/LandingPage.tsx
@@ -60,11 +60,11 @@ const LandingPage: React.FC = () => {
           <h1 style={{paddingTop: "10vh"}}>Hello, Welcome to Geo Pick Points!</h1>
           <p style={{ marginTop: "1rem", fontSize: "1.2rem", color: "#333" }}>
           
-          <strong>What is Geo Pick Points?</strong>
+          <br> <strong>What is Geo Pick Points?</strong></br>
 Geo Pick Points is an exciting game that takes you on a virtual scavenger hunt. You can play solo or in teams, exploring interesting locations while collecting points. Whether cooperative or competitive, the goal is fun and adventure!
-<br></br>
+
 <strong>The best part?</strong>
-<br></br>
+
 Discover new surroundings, claim stations, and compete against each other – and don’t forget to dress appropriately for the weather, as the adventure takes place outdoors!
   </p>
           <HostGame onClick={handleGameStartClick} />
